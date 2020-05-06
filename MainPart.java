3f9e8584fb3a02f3c2dec9cf122a3e4d99d9da18
YukiHime <36097203+Numseven@users.noreply.github.com>
2020-05-06 14:30:18 +0800

    新增視窗
    現在會跳出新視窗了
    按下右上角的X會直接停止程序

--- MainPart.java
+++
@@ -1,7 +0,0 @@
-package tenseconds;
-
-public class MainPart{
-    public static void main(String[] args) {
-        System.out.println("It's fucking title!!!!");
-    }
-}
---
+++ MajorPart.java
@@ -0,0 +1,7 @@
+package tenseconds;
+
+public class MajorPart{
+    public static void main(String[] args) {
+        Frame major = new Frame();
+    }
+}
