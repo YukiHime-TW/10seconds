264d2b53ecfc939ad3fa2b40f75762a7be2f88f1
YukiHime <36097203+Numseven@users.noreply.github.com>
2020-05-11 22:09:05 +0800

    新增了很多東西

--- Level.java
+++ Level.java
@@ -4,4 +4,16 @@ import java.util.Timer;
 
 public class Level {
     //地圖的生成
+
+    public void tick() {
+		for (Entity e : entities) {
+			e.tick();
+		}
+		for (Tile t : Tile.tiles) {
+			if (t == null) {
+				break;
+			}
+			t.tick();
+		}
+	}
 }
--- MajorPart.java
+++ MajorPart.java
@@ -21,6 +21,10 @@ public class MajorPart extends Canvas implements Runnable {
 
     public boolean running = false;
 
+    public int tickCount = 0;
+
+    public static Level level;
+
     public MajorPart() {
 
         // 處理圖片用的
@@ -53,8 +57,51 @@ public class MajorPart extends Canvas implements Runnable {
 
     @Override
     public void run() {
+        long lastTime = System.nanoTime();
+        double nsPerTick = 1000000000 / 60D;
 
+        int ticks = 0;
+        int frames = 0;
+
+        long lastTimer = System.currentTimeMillis();
+        double delta = 0;
+
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / nsPerTick;
+            lastTime = now;
+            boolean shouldRender = true;
+
+            while (delta >= 1) {
+                ticks++;
+                tick();
+                delta -= 1;
+                shouldRender = true;
+            }
+            try {
+                Thread.sleep(2);
+            } catch (InterruptedException e) {
+
+                e.printStackTrace();
+            }
+            if (shouldRender) {
+                frames++;
+                render();
+            }
+
+            if (System.currentTimeMillis() - lastTimer >= 1000) {
+                lastTimer += 1000;
+                System.out.println("" + ticks + " ticks, " + frames + " frames");
+                frames = 0;
+                ticks = 0;
+            }
+        }
     }
+
+    public void tick() {
+		tickCount++;
+		level.tick();
+	}
 
     public static void main(String[] args) {
         new MajorPart().start();
