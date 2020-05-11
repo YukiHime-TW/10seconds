f652b5a9e22eaee1ddc9e24c38bd2b137058586c
YukiHime <36097203+Numseven@users.noreply.github.com>
2020-05-11 22:02:30 +0800

    刪除了frame.java
    因為發現 好像沒什麼用

--- MajorPart.java
+++ MajorPart.java
@@ -10,27 +10,53 @@ import java.awt.image.DataBufferInt;
 import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import java.awt.Image;
+import java.awt.Toolkit;
 
 public class MajorPart extends Canvas implements Runnable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public boolean running = false;
+    public boolean running = false;
 
-    public MajorPart(){
-        Frame major = new Frame();
+    public MajorPart() {
+
+        // 處理圖片用的
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+
+        JFrame jframe = new JFrame("時間勇者:Take A Breath");
+
+        jframe.setIconImage(toolkit.getImage("src/icon.png"));
+
+        jframe.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        jframe.setLocationRelativeTo(null);
+
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        jframe.setLayout(new BorderLayout());
+
+        jframe.add(this, BorderLayout.CENTER);
+
+        jframe.pack();
+
+        jframe.setVisible(true);
+
     }
 
     public synchronized void start() {
-		running = true;
-		new Thread(this).start();
+        running = true;
+        new Thread(this).start();
     }
-    
+
     @Override
-    public void run(){
+    public void run() {
 
     }
+
     public static void main(String[] args) {
-        new MajorPart();
+        new MajorPart().start();
     }
 }
