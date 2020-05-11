5a70b201741ea4a1efcc1ad782f8d57a89f4a227
YukiHime <36097203+Numseven@users.noreply.github.com>
2020-05-11 21:42:39 +0800

    Bug 修復
    解決了addKeyListener()會報錯的問題

--- MajorPart.java
+++ MajorPart.java
@@ -8,9 +8,29 @@ import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class MajorPart {
-    public static void main(String[] args) {
+public class MajorPart extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = 1L;
+
+	public boolean running = false;
+
+    public MajorPart(){
         Frame major = new Frame();
+    }
+
+    public synchronized void start() {
+		running = true;
+		new Thread(this).start();
+    }
+    
+    @Override
+    public void run(){
+
+    }
+    public static void main(String[] args) {
+        new MajorPart();
     }
 }
