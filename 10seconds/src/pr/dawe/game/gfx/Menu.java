3baf0478fb58d25482be5f9f344555a41e01069c
hungchun000 <hungchun000@users.noreply.github.com>
2020-06-02 21:43:36 +0800

    Update Menu.java

---
+++ 10seconds/src/pr/dawe/game/gfx/Menu.java
@@ -0,0 +1,73 @@
+//進入點
+package pr.dawe.game.gfx;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+
+import javax.swing.*;
+import javax.swing.event.*;
+
+import pr.dawe.game.Game;
+import pr.dawe.game.level.Level;
+
+public class Menu extends JFrame {
+
+	private JButton jButton1 = new JButton();
+	public static boolean enterLevel;
+	public static boolean enterDungeonForest;
+	public static boolean credtis;
+	public static boolean running = false;
+	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+	private int credits = 0;
+
+	public Menu(String title) {
+
+		super(title);
+		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		int frameWidth = 650;
+		int frameHeight = 500;
+		setSize(frameWidth, frameHeight);
+		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+		int x = (d.width - getSize().width) / 2;
+		int y = (d.height - getSize().height) / 2;
+		setLocation(x, y);
+		setResizable(false);
+		Container cp = getContentPane();
+		cp.setLayout(null);
+
+		jButton1.setBounds(168, 80, 305, 57);
+		jButton1.setText("Start new Level");
+		jButton1.setMargin(new Insets(2, 2, 2, 2));
+		jButton1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				jButton1_ActionPerformed(evt);
+			}
+		});
+		jButton1.setBackground(Color.WHITE);
+		jButton1.setBorder(BorderFactory.createEtchedBorder(0, Color.GREEN, Color.BLACK));
+		cp.add(jButton1);
+		cp.setBackground(new Color(0xFFC800));
+
+		setVisible(true);
+	}
+
+	public void jButton1_ActionPerformed(ActionEvent evt) { // ENTER LEVEL
+		if (running == false) {
+			Game.main(null);
+			closeMenu();
+		} else {
+			System.out.println("Already running!");
+		}
+	}
+
+	public void closeMenu() {
+		WindowEvent wev = new WindowEvent(this, WindowEvent.WINDOW_CLOSING);
+		Toolkit.getDefaultToolkit().getSystemEventQueue().postEvent(wev);
+	}
+
+	public static void main(String[] args) {
+		new Menu("時間勇者:Take a breath");
+	}
+
+}
--- World.java
+++
@@ -1,5 +0,0 @@
-package bin.tenseconds.level;
-
-public class World extends Level{
-    //移動中畫面
-}
