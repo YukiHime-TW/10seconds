f5dbe311ad95b12cd272c8cadd8b7c12ec0e6825
YukiHime <36097203+Numseven@users.noreply.github.com>
2020-06-02 19:36:33 +0800

    Update MajorPart.java

--- MajorPart.java
+++ MajorPart.java
@@ -1,19 +1,9 @@
 package bin.tenseconds.major;
 
-import java.awt.BorderLayout;
-import java.awt.Canvas;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.image.BufferStrategy;
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferInt;
 import java.util.*;
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import java.awt.Image;
-import java.awt.Toolkit;
 
 public class MajorPart extends Canvas implements Runnable {
 
