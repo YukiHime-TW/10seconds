256e265876f5c256590dc5d1efe90b1a69dabdee
YukiHime <36097203+Numseven@users.noreply.github.com>
2020-06-14 16:18:38 +0800

    Update Player.java

--- 10seconds/src/pr/dawe/game/entities/Player.java
+++ 10seconds/src/pr/dawe/game/entities/Player.java
@@ -127,6 +127,33 @@ public class Player extends Mob {
 		}
 		tickCount++;
 	}
+	public boolean isMob(int xa, int ya) {
+		int xMin = 0;
+		int xMax = 7;
+		int yMin = 3;
+		int yMax = 7;
+		for (int x = xMin; x < xMax; x++) {
+			if (isSolidTile(xa, ya, x, yMin)) {
+				return true;
+			}
+		}
+		for (int x = xMin; x < xMax; x++) {
+			if (isSolidTile(xa, ya, x, yMax)) {
+				return true;
+			}
+		}
+		for (int y = yMin; y < yMax; y++) {
+			if (isSolidTile(xa, ya, xMin, y)) {
+				return true;
+			}
+		}
+		for (int y = yMin; y < yMax; y++) {
+			if (isSolidTile(xa, ya, xMax, y)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 	public void render(Screen screen) {
 		int xTile = 0;
